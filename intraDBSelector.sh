2993d1d3f4cea3738b0541c4a0b95b10805ff722
mitchem890@gmail.com <mitchem890@gmail.com>
2017-07-26 12:20:33 -0500

    Added intraDBSelector File to allow Simplier download of intraDB data

---
+++ intraDBSelector.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DOWNLOAD_LOCATION=/scratch/mjeffers/DMCCPILOT/DOWNLOADS
+
+read -p "ENTER SUBJECT: " SUBJ; 
+echo $SUBJ
+read -p "ENTER SESSION: " SESSION; 
+echo $SESSION
+read -p "ENTER SCAN NUMBERS: " SCANS;
+echo $SCANS
+
+pushd $DOWNLOAD_LOCATION
+mkdir -p $SUBJ/unprocessed/3T
+pushd $SUBJ/unprocessed/3T
+
+
+curl -k -n https://intradb.humanconnectome.org/data/projects/DMCC_Phase2/subjects/${SUBJ}/experiments/${SUBJ}_${SESSION}/scans/${SCANS}/resources/NIFTI/files?format=zip>tmp.zip && unzip tmp.zip && rm tmp.zip
+
+popd
+popd
