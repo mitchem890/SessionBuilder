b62feb99eafdd65373d01ea90f04ce8aab868ec5
Mitch Jeffers <mitchem890@gmail.com>
2017-08-11 10:23:28 -0500

    added option to choose phase

--- intraDBDownloader.sh
+++ intraDBDownloader.sh
@@ -2,12 +2,11 @@
 
 DOWNLOAD_LOCATION=/scratch/${USERNAME}/DMCCPILOT/DOWNLOADS
 HOST='intradb.humanconnectome.org'
-PROJ='DMCC_Phase2'
-#read -p "ENTER PROJECT: " PROJ;
+read -p "ENTER PROJECT (DMCC_Phase2 or DMCC_Phase3): " PROJ;
 read -p "ENTER SUBJECT: " SUBJ;
 read -p "ENTER SESSION: " session;
 pushd $DOWNLOAD_LOCATION
-mkdir -p $SUBJ/unprocessed/3T
+mkdir -p $SUBJ/unprocessed/3T/${SUBJ}_${session}
 pushd $SUBJ/unprocessed/3T
 
 for RESOURCE in $(IFS=% && curl -s -k -n https://intradb.humanconnectome.org/data/projects/${PROJ}/subjects/${SUBJ}/experiments/${SUBJ}_${session}/scans?format=csv | grep -F -e'mrScanData' | grep -F -e 'tfMRI' -e 'T1w' -e 'T2w' -e 'SpinEchoFieldMap' -e 'rfMRI'| cut -d, -f2,4,7; unset IFS); do
@@ -21,7 +20,7 @@ printf "%-5s  %-35s  %-20s\n" "${scan_num}" "${scan_name}" "${scan_qa}"
 
 ## TODO:
 ##  echo "curl -s -k -n https://$HOST/data/projects/$PROJ/subjects/${SUBJ}/experiments/${SUBJ}_3T/resources/$RESOURCE/files?format=zip"
-	curl -k -n https://${HOST}/data/projects/${PROJ}/subjects/${SUBJ}/experiments/${SUBJ}_${session}/scans/${scan_num}/resources/NIFTI/files?format=zip > tmp.zip && unzip tmp.zip && rm tmp.zip
+	curl -k -n https://${HOST}/data/projects/${PROJ}/subjects/${SUBJ}/experiments/${SUBJ}_${session}/scans/${scan_num}/resources/NIFTI/files?format=zip > ${SUBJ}_${session}/tmp.zip && unzip ${SUBJ}_${session}/tmp.zip && rm ${SUBJ}_${session}/tmp.zip
 ## TODO: if scan_qa=unusable, break
 ## TODO: instead of ‘for x in $(curl ..)’, create a textfile and ‘while read < file’
 done
